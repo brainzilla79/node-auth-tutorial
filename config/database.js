15c84c5abf9964dbe79b9af400390f47b657ff15
Alex <alexanderhapp@me.com>
2018-03-08 15:21:54 -0800

    Finish db config

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: 'mongodb://<dbuser>:<dbpassword>@ds143231.mlab.com:43231/ahapp' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+  url: 'mongodb://alex:mrspuppy@ds143231.mlab.com:43231/ahapp' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 };
